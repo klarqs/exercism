d7b713056aa58ae911b10a224a5c473a5cae38a4
Kolawole Paul <63057921+kola-rt@users.noreply.github.com>
2023-02-25 23:33:50 +0100

    Update difference_of_squares.dart

--- dart/difference-of-squares/lib/difference_of_squares.dart
+++ dart/difference-of-squares/lib/difference_of_squares.dart
@@ -1,13 +1,16 @@
 class DifferenceOfSquares {
   int squareOfSum(int number) {
     return 0;
+    
   }
 
   int sumOfSquares(int number) {
     return 0;
+    
   }
 
   int differenceOfSquares(int number) {
     return 0;
+    
   }
 }
